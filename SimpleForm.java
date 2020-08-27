27447650362dc67cd88461022ef391628f213b1e
sky-adams <skyadams@sbunified.org>
2020-08-27 14:44:54 -0700

    Fix indentation

--- SimpleForm.java
+++ SimpleForm.java
@@ -19,37 +19,37 @@ public class SimpleForm extends Application
         stage.setTitle("JavaFX Welcome");
         
         GridPane grid = new GridPane();
-		grid.setAlignment(Pos.CENTER);
-		grid.setHgap(10);
-		grid.setVgap(10);
-		grid.setPadding(new Insets(25, 25, 25, 25));
+	grid.setAlignment(Pos.CENTER);
+	grid.setHgap(10);
+	grid.setVgap(10);
+	grid.setPadding(new Insets(25, 25, 25, 25));
 		
-		Text scenetitle = new Text("Welcome");
-		grid.add(scenetitle, 0, 0, 2, 1);
+	Text scenetitle = new Text("Welcome");
+	grid.add(scenetitle, 0, 0, 2, 1);
 		
-		Label userName = new Label("User Name:");
-		grid.add(userName, 0, 1);
+	Label userName = new Label("User Name:");
+	grid.add(userName, 0, 1);
 
-		TextField userTextField = new TextField();
-		grid.add(userTextField, 1, 1);
+	TextField userTextField = new TextField();
+	grid.add(userTextField, 1, 1);
 
-		Label pw = new Label("Password:");
-		grid.add(pw, 0, 2);
+	Label pw = new Label("Password:");
+	grid.add(pw, 0, 2);
 
-		PasswordField pwBox = new PasswordField();
-		grid.add(pwBox, 1, 2);
+	PasswordField pwBox = new PasswordField();
+	grid.add(pwBox, 1, 2);
 		
-		Button btn = new Button("Sign in");
-		HBox hbBtn = new HBox(10);
-		hbBtn.setAlignment(Pos.BOTTOM_RIGHT); 
-		hbBtn.getChildren().add(btn);
-		grid.add(hbBtn, 1, 4);
+	Button btn = new Button("Sign in");
+	HBox hbBtn = new HBox(10);
+	hbBtn.setAlignment(Pos.BOTTOM_RIGHT); 
+	hbBtn.getChildren().add(btn);
+	grid.add(hbBtn, 1, 4);
 		
-		Text actiontarget = new Text();
+	Text actiontarget = new Text();
         grid.add(actiontarget, 1, 6);
 
-		Scene scene = new Scene(grid, 300, 275);
-		stage.setScene(scene);
+	Scene scene = new Scene(grid, 300, 275);
+	stage.setScene(scene);
         stage.show();
     }
 
